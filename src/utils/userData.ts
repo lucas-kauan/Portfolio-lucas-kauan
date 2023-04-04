b447e1c357051e60fd6ed271d99746c1196c4847
Kauan <kauancompositor@outlook.com>
2023-04-04 14:59:59 -0300

    Add informations about me

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+  nameUser: "Lucas Kauan",
+  githubUser: "lucas-kauan",
+  linkedinUser: "lucas-kauan-nunes",
+  whatsappNumber: "+5588988810175",
 };
